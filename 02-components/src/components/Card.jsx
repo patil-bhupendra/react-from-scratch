ab28b00d30f2adef4fdc08e8c29d298de86b4524
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-01-23 22:57:21 +0530

    Practice React components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Bhupendra Patil</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, quo?</p>
+    </div>
+  );
+};
+
+export default Card;
