27dcea1a2dbf3bbe8e7ebb43ffbf691da1b4ef15
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-23 10:40:33 +0530

    Add component creation and usage examples in React

---
+++ 03-components/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card">
+      <h1>Bhupendra Patil</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eos.</p>
+    </div>
+  );
+}
+
+export default Card;
