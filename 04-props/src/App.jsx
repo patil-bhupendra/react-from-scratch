ffb697c4cf95c0425032d94afff3104e53252d52
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-24 19:14:00 +0530

    Practice React props with reusable Card component

--- 04-props/src/App.jsx
+++ 04-props/src/App.jsx
@@ -4,7 +4,21 @@ import Card from "./components/Card";
 const App = () => {
   return (
     <div className="parent">
-      <Card user = "Aman"/>
+      <Card
+        user="Bhupendra"
+        age={18}
+        img="https://avatars.githubusercontent.com/u/158457928?v=4"
+      />
+      <Card
+        user="Dhiraj"
+        age={24}
+        img="https://avatars.githubusercontent.com/u/139003559?v=4"
+      />
+      <Card
+        user="Abhishek"
+        age={19}
+        img="https://images.unsplash.com/photo-1504139969750-3244f9258fce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"
+      />
     </div>
   );
 };
--- 04-props/src/components/Card.jsx
+++ 04-props/src/components/Card.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-      <img
-        src="https://images.unsplash.com/photo-1665685153413-13a1e704450e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI2fHx8ZW58MHx8fHx8"
-        alt=""
-      />
-      <h1>Bhupendra Patil</h1>
+      <img src={props.img} alt="" />
+      <h1>
+        {props.user},{props.age}
+      </h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       <button>View Profile</button>
     </div>
