ba267eb30fea69bbd6e5b9df7245ef6db42d15d0
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-25 17:04:49 +0530

    Add React CSS styling practice using CSS Modules

---
+++ 06-css-styling/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h3>Bhupendra</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
