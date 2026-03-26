29d8b62bf7d94e18ab5319a311e412d53c00805c
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-26 16:53:21 +0530

    Add React functions and event handling practice

---
+++ 09-functions/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  
+  return (
+    <div>
+      <button onClick={function(){
+        console.log('Hello guys');
+        
+      }}>click here</button>
+    </div>
+  )
+}
+
+export default App
