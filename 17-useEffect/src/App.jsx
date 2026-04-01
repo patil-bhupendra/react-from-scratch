9690829ef1c1bb542e5d0867ddd4d5542ac927ba
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-04-01 14:23:57 +0530

    Add useEffect demo with separate effects for state changes

--- 17-useEffect/src/App.jsx
+++ 17-useEffect/src/App.jsx
@@ -1,26 +1,47 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [num, setNum] = useState(0);
-  const [num2, setNum2] = useState(100);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
 
-  useEffect(function () {
-    console.log("use effect is running...");
-  }, [num]);
+  function aChanging() {
+    console.log("A ki value change ho gai");
+  }
+  function bChanging() {
+    console.log("B ki value change ho gai");
+  }
+
+  useEffect(
+    function () {
+      aChanging();
+    },
+    [a],
+  );
+
+  useEffect(
+    function () {
+      bChanging();
+    },
+    [b],
+  );
 
   return (
     <div>
-      <h1> num {num}</h1>
-      <h1> num2 {num2}</h1>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
       <button
-        onMouseEnter={() => {
-          setNum(num + 1);
-        }}
-        onMouseLeave={() => {
-          setNum2(num2 + 10);
+        onClick={() => {
+          setA(a + 1);
         }}
       >
-        Hover
+        Change A
+      </button>
+      <button
+        onClick={() => {
+          setB(b - 1);
+        }}
+      >
+        Change B
       </button>
     </div>
   );
